87d686ea5c7c8d85aa8fed01bceaf9fb45c3ff45
jmalmsten <jmalmsten85@gmail.com>
2026-04-04 19:38:14 +0100

    removed old vestigual installtion instructions

--- force_full_rgb.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# VOP Utility: Force HDMI-A-1 to Full RGB (0-255)
-# This overrides the Limited Range (16-235) default on Pi 5
-/usr/bin/kmstest -c HDMI-A-1 -P "Broadcast RGB=1" < /dev/null
